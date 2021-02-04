4b629249d32b4d0d24e95e739062fd734a484a06
Algirdas <algirdas@zyro.com>
2021-02-05 01:52:02 +0200

    Switch to pagespeed api

--- functions/index.js
+++ functions/index.js
@@ -1,45 +1,38 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-const lighthouse = require('lighthouse');
-const chromeLauncher = require('chrome-launcher');
-const moment = require('moment');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const lighthouse = require("lighthouse");
+const chromeLauncher = require("chrome-launcher");
+const moment = require("moment");
+const axios = require("axios");
+
+const API_URL = "https://www.googleapis.com/pagespeedonline/v5/runPagespeed";
 
 admin.initializeApp();
 
 const getAvg = (arr) => arr.reduce((a, b) => a + b, 0) / arr.length;
 
-exports.insight = functions.https.onRequest(async (request, respoonse) => {
+exports.insight = functions.https.onRequest(async (request, response) => {
   const { url, iterations } = request.query || {};
-  const scores = [];
 
-  const chrome = await chromeLauncher.launch({chromeFlags: ['--headless']});
-  
-  const options = {
-    logLevel: 'info',
-    output: 'json',
-    onlyCategories: ['performance'],
-    port: chrome.port
-  };
+  const requests = Array.from({ length: iterations || 1 }).map(() =>
+    axios.get(`${API_URL}?strategy=mobile&url=${url}&key=${functions.config().insight.key}`)
+  );
 
-  for (i of Array.from({length: iterations || 3})) {
-      const { lhr }  = await lighthouse(url, options);
-      const { score } = lhr.categories.performance;
-      scores.push(score * 100);
-  }
-
-  await chrome.kill();
-
-  const average = getAvg(scores);
+  const responses = await Promise.all(requests);
+  const scores = responses.map(
+    ({ data }) => data.lighthouseResult.categories.performance.score
+  );
 
   const entry = {
       url,
-      average,
+      average: getAvg(scores) * 100,
       date: moment().format('YYYY-MM-DD hh:mm:ss')
   }
+
   await admin.firestore().collection('insigths').add(entry);
 
   const { docs } = await admin.firestore().collection('insigths').get()
   const allEntries = docs.map(doc => doc.data());
 
-  respoonse.json(allEntries);
+  response.json(allEntries);
 });
